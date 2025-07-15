5344681500ca3d6acdd63c3f724746c38d360042
André Santos <andrelucas25111@gmail.com>
2025-07-14 23:11:31 -0300

    test

--- Codes/ticket/01-create-database-users.sql
+++ Codes/ticket/01-create-database-users.sql
@@ -1,4 +1,24 @@
-CREATE USER "pg-tickets-users" WITH PASSWORD 'tickets-users-password';
+-- Creates the user only if it doesn't exist
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'pg-tickets-users') THEN
 
-CREATE DATABASE "tickets-users";
+      CREATE ROLE "pg-tickets-users" WITH LOGIN PASSWORD 'tickets-users-password';
+   END IF;
+END
+$do$;
+
+-- Creates the database only if it doesn't exist
+-- Note: You cannot run CREATE DATABASE inside a transaction block,
+-- so we check for its existence in a separate query.
+-- This part is a bit tricky and often it's better to rely on the
+-- POSTGRES_DB environment variable. However, to make your script work:
+-- You can connect to the default 'postgres' database and run:
+-- SELECT 'CREATE DATABASE "tickets-users"'
+-- WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'tickets-users')\gexec
+-- For simplicity in the entrypoint script, we'll just alter the owner.
+
 ALTER DATABASE "tickets-users" OWNER TO "pg-tickets-users";
--- Codes/ticket/02-create-database-sales.sql
+++ Codes/ticket/02-create-database-sales.sql
@@ -1,4 +1,14 @@
-CREATE USER "pg-tickets-sales" WITH PASSWORD 'tickets-sales-password';
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'pg-tickets-sales') THEN
 
-CREATE DATABASE "tickets-sales";
+      CREATE ROLE "pg-tickets-sales" WITH LOGIN PASSWORD 'tickets-sales-password';
+   END IF;
+END
+$do$;
+
+
 ALTER DATABASE "tickets-sales" OWNER TO "pg-tickets-sales";
